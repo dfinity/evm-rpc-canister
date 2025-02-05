84a00a9e088bb3a04fcd6d9d9916110ab16c0996
gregorydemay <gregory.demay@dfinity.org>
2025-02-05 21:12:47 +0100

    copied over cycles cost calculation

--- canjsonrpc/src/cycles/mod.rs
+++ canjsonrpc/src/cycles/mod.rs
@@ -13,16 +13,60 @@ pub struct DefaultRequestCost {
 }
 
 impl DefaultRequestCost {
+    pub const DEFAULT_MAX_RESPONSE_BYTES: u64 = 2_000_000;
+
     pub fn new(num_nodes_in_subnet: u32) -> Self {
         DefaultRequestCost {
             num_nodes_in_subnet,
         }
     }
+
+    fn base_fee(&self) -> u128 {
+        3_000_000_u128
+            .saturating_add(60_000_u128.saturating_mul(self.num_nodes_as_u128()))
+            .saturating_mul(self.num_nodes_as_u128())
+    }
+
+    fn request_fee(&self, bytes: u128) -> u128 {
+        400_u128
+            .saturating_mul(self.num_nodes_as_u128())
+            .saturating_mul(bytes)
+    }
+
+    fn response_fee(&self, bytes: u128) -> u128 {
+        800_u128
+            .saturating_mul(self.num_nodes_as_u128())
+            .saturating_mul(bytes)
+    }
+
+    fn num_nodes_as_u128(&self) -> u128 {
+        self.num_nodes_in_subnet as u128
+    }
 }
 
 impl EstimateRequestCyclesCost for DefaultRequestCost {
     fn cycles_cost(&self, request: &CanisterHttpRequestArgument) -> u128 {
-        todo!()
+        let payload_body_bytes = request
+            .body
+            .as_ref()
+            .map(|body| body.len())
+            .unwrap_or_default();
+        let extra_payload_bytes = request.url.len()
+            + request
+                .headers
+                .iter()
+                .map(|header| header.name.len() + header.value.len())
+                .sum::<usize>()
+            + request.transform.as_ref().map_or(0, |transform| {
+                transform.function.0.method.len() + transform.context.len()
+            });
+        let max_response_bytes = request
+            .max_response_bytes
+            .unwrap_or(Self::DEFAULT_MAX_RESPONSE_BYTES);
+        let request_bytes = (payload_body_bytes + extra_payload_bytes) as u128;
+        self.base_fee()
+            + self.request_fee(request_bytes)
+            + self.response_fee(max_response_bytes as u128)
     }
 }
 
