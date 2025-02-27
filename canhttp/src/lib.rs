fe94e6a85887267569247d93845c47feb3e9c728
gregorydemay <gregory.demay@dfinity.org>
2025-02-27 17:15:40 +0100

    docs: added http top-level docs

--- canhttp/src/lib.rs
+++ canhttp/src/lib.rs
@@ -3,8 +3,7 @@
 //! leveraging the modularity of the [tower framework](https://rust-lang.guide/guide/learn-async-rust/tower.html).
 
 #![forbid(unsafe_code)]
-//TODO : re-allow this and write the docs
-// #![forbid(missing_docs)]
+#![forbid(missing_docs)]
 
 pub use client::{Client, IcError};
 pub use cycles::{
