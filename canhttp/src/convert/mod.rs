c46b45763677e1afabfda00357088e7a253b63de
gregorydemay <gregory.demay@dfinity.org>
2025-03-04 10:55:54 +0100

    rename generic parameters

--- canhttp/src/convert/mod.rs
+++ canhttp/src/convert/mod.rs
@@ -1,4 +1,4 @@
-use futures_util::future::Either;
+use futures_util::future;
 use pin_project::pin_project;
 use std::fmt::Debug;
 use std::future::Future;
@@ -15,36 +15,37 @@ pub trait Convert<Input> {
 }
 
 #[derive(Debug, Clone)]
-pub struct ConvertResponseLayer<F> {
-    filter: F,
+pub struct ConvertResponseLayer<C> {
+    converter: C,
 }
 
 #[derive(Debug, Clone)]
-pub struct ConvertResponse<S, F> {
+pub struct ConvertResponse<S, C> {
     inner: S,
-    filter: F,
+    converter: C,
 }
 
-impl<S, F: Clone> Layer<S> for ConvertResponseLayer<F> {
-    type Service = ConvertResponse<S, F>;
+impl<S, C: Clone> Layer<S> for ConvertResponseLayer<C> {
+    type Service = ConvertResponse<S, C>;
 
     fn layer(&self, inner: S) -> Self::Service {
         Self::Service {
             inner,
-            filter: self.filter.clone(),
+            converter: self.converter.clone(),
         }
     }
 }
 
-impl<S, Request, Response, NewResponse, F> Service<Request> for ConvertResponse<S, F>
+impl<S, Request, Response, NewResponse, Converter> Service<Request>
+    for ConvertResponse<S, Converter>
 where
     S: Service<Request, Response = Response>,
-    F: Convert<Response, Output = NewResponse> + Clone,
-    F::Error: Into<S::Error>,
+    Converter: Convert<Response, Output = NewResponse> + Clone,
+    Converter::Error: Into<S::Error>,
 {
     type Response = NewResponse;
     type Error = S::Error;
-    type Future = ResponseFuture<S::Future, F>;
+    type Future = ResponseFuture<S::Future, Converter>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.inner.poll_ready(cx)
@@ -53,16 +54,16 @@ where
     fn call(&mut self, req: Request) -> Self::Future {
         ResponseFuture {
             response_future: self.inner.call(req),
-            filter: self.filter.clone(),
+            converter: self.converter.clone(),
         }
     }
 }
 
 #[pin_project]
-pub struct ResponseFuture<F, Filter> {
+pub struct ResponseFuture<F, Converter> {
     #[pin]
     response_future: F,
-    filter: Filter,
+    converter: Converter,
 }
 
 impl<F, Filter, Response, NewResponse, Error> Future for ResponseFuture<F, Filter>
@@ -78,7 +79,9 @@ where
         let result_fut = this.response_future.poll(cx);
         match result_fut {
             Poll::Ready(result) => match result {
-                Ok(response) => Poll::Ready(this.filter.try_convert(response).map_err(Into::into)),
+                Ok(response) => {
+                    Poll::Ready(this.converter.try_convert(response).map_err(Into::into))
+                }
                 Err(err) => Poll::Ready(Err(err)),
             },
             Poll::Pending => Poll::Pending,
@@ -87,60 +90,63 @@ where
 }
 
 pub trait ConvertServiceBuilder<L> {
-    fn convert_request<F>(self, f: F) -> ServiceBuilder<Stack<ConvertRequestLayer<F>, L>>;
-    fn convert_response<F>(self, f: F) -> ServiceBuilder<Stack<ConvertResponseLayer<F>, L>>;
+    fn convert_request<C>(self, f: C) -> ServiceBuilder<Stack<ConvertRequestLayer<C>, L>>;
+    fn convert_response<C>(self, f: C) -> ServiceBuilder<Stack<ConvertResponseLayer<C>, L>>;
 }
 
 impl<L> ConvertServiceBuilder<L> for ServiceBuilder<L> {
-    fn convert_request<F>(self, f: F) -> ServiceBuilder<Stack<ConvertRequestLayer<F>, L>> {
-        self.layer(ConvertRequestLayer { filter: f })
+    fn convert_request<C>(self, converter: C) -> ServiceBuilder<Stack<ConvertRequestLayer<C>, L>> {
+        self.layer(ConvertRequestLayer { converter })
     }
 
-    fn convert_response<F>(self, f: F) -> ServiceBuilder<Stack<ConvertResponseLayer<F>, L>> {
-        self.layer(ConvertResponseLayer { filter: f })
+    fn convert_response<C>(
+        self,
+        converter: C,
+    ) -> ServiceBuilder<Stack<ConvertResponseLayer<C>, L>> {
+        self.layer(ConvertResponseLayer { converter })
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct ConvertRequestLayer<F> {
-    filter: F,
+pub struct ConvertRequestLayer<C> {
+    converter: C,
 }
 
 #[derive(Debug, Clone)]
-pub struct ConvertRequest<S, F> {
+pub struct ConvertRequest<S, C> {
     inner: S,
-    filter: F,
+    converter: C,
 }
 
-impl<S, F: Clone> Layer<S> for ConvertRequestLayer<F> {
-    type Service = ConvertRequest<S, F>;
+impl<S, C: Clone> Layer<S> for ConvertRequestLayer<C> {
+    type Service = ConvertRequest<S, C>;
 
     fn layer(&self, inner: S) -> Self::Service {
         Self::Service {
             inner,
-            filter: self.filter.clone(),
+            converter: self.converter.clone(),
         }
     }
 }
 
-impl<S, F, Request, NewRequest, Error> Service<NewRequest> for ConvertRequest<S, F>
+impl<S, Converter, Request, NewRequest, Error> Service<NewRequest> for ConvertRequest<S, Converter>
 where
-    F: Convert<NewRequest, Output = Request>,
+    Converter: Convert<NewRequest, Output = Request>,
     S: Service<Request, Error = Error>,
-    F::Error: Into<Error>,
+    Converter::Error: Into<Error>,
 {
     type Response = S::Response;
     type Error = S::Error;
-    type Future = Either<S::Future, futures_util::future::Ready<Result<S::Response, S::Error>>>;
+    type Future = future::Either<S::Future, future::Ready<Result<S::Response, S::Error>>>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.inner.poll_ready(cx)
     }
 
     fn call(&mut self, new_req: NewRequest) -> Self::Future {
-        match self.filter.try_convert(new_req) {
-            Ok(request) => Either::Left(self.inner.call(request)),
-            Err(err) => Either::Right(futures_util::future::ready(Err(err.into()))),
+        match self.converter.try_convert(new_req) {
+            Ok(request) => future::Either::Left(self.inner.call(request)),
+            Err(err) => future::Either::Right(future::ready(Err(err.into()))),
         }
     }
 }
