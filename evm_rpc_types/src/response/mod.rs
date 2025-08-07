d5d34132232f7b9a9da9f983bc48896a414cdafd
Louis Pahlavi <louis.pahlavi@dfinity.org>
2025-08-07 10:55:28 +0200

    XC-412: Add more type conversions to `alloy`

--- evm_rpc_types/src/response/mod.rs
+++ evm_rpc_types/src/response/mod.rs
@@ -1,4 +1,4 @@
-use crate::{Hex, Hex20, Hex256, Hex32, HexByte, Nat256};
+use crate::{Hex, Hex20, Hex256, Hex32, HexByte, Nat256, RpcError, ValidationError};
 use candid::CandidType;
 use serde::{Deserialize, Serialize};
 
@@ -70,20 +70,30 @@ pub struct LogEntry {
 
 #[cfg(feature = "alloy")]
 impl TryFrom<LogEntry> for alloy_rpc_types::Log {
-    type Error = String;
+    type Error = RpcError;
 
     fn try_from(entry: LogEntry) -> Result<Self, Self::Error> {
         Ok(Self {
             inner: alloy_primitives::Log {
-                address: entry.address,
-                data: alloy_primitives::LogData::new(entry.topics, entry.data).ok_or()?,
+                address: alloy_primitives::Address::from(entry.address),
+                data: alloy_primitives::LogData::new(
+                    entry
+                        .topics
+                        .into_iter()
+                        .map(|topic| alloy_primitives::B256::from(topic))
+                        .collect(),
+                    alloy_primitives::Bytes::from(entry.data),
+                )
+                .ok_or(RpcError::ValidationError(ValidationError::Custom(
+                    "Invalid log data".to_string(),
+                )))?,
             },
-            block_hash: entry.block_hash,
-            block_number: entry.block_number,
-            block_timestamp: entry.block_timestamp,
-            transaction_hash: entry.transaction_hash,
-            transaction_index: entry.transaction_index,
-            log_index: entry.log_index,
+            block_hash: entry.block_hash.map(alloy_primitives::BlockHash::from),
+            block_number: entry.block_number.map(u64::try_from).transpose()?,
+            block_timestamp: None,
+            transaction_hash: entry.transaction_hash.map(alloy_primitives::TxHash::from),
+            transaction_index: entry.transaction_index.map(u64::try_from).transpose()?,
+            log_index: entry.log_index.map(u64::try_from).transpose()?,
             removed: entry.removed,
         })
     }
