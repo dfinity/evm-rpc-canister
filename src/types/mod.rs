e91f78f82870d380bc0e13eeb82c4dc8ebacbd4c
Louis Pahlavi <louis.pahlavi@dfinity.org>
2025-09-05 14:08:09 +0200

    Add new metric field to Candid file

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -164,7 +164,7 @@ pub struct Metrics {
     pub cycles_charged: HashMap<(MetricRpcMethod, MetricRpcHost), u128>,
     #[serde(rename = "errHttpOutcall")]
     pub err_http_outcall: HashMap<(MetricRpcMethod, MetricRpcHost, LegacyRejectionCode), u64>,
-    #[serde(rename = "maxResponseSizeExceeded")]
+    #[serde(rename = "errMaxResponseSizeExceeded")]
     pub err_max_response_size_exceeded: HashMap<(MetricRpcMethod, MetricRpcHost), u64>,
 }
 
