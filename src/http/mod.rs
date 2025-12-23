8f4163098146183a73d5159090d8effb711bf915
Louis Pahlavi <louis.pahlavi@dfinity.org>
2025-12-23 15:26:56 +0100

    Make `Error` type generic for client

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -3,7 +3,14 @@ use crate::{
     memory::{get_num_subnet_nodes, is_demo_active, next_request_id},
     util::canonicalize_json,
 };
-use canhttp::http::json::{CreateJsonRpcIdFilter, HttpBatchJsonRpcRequest, JsonRpcRequest};
+use canhttp::cycles::ChargeCallerError;
+use canhttp::http::json::{
+    ConsistentResponseIdFilterError, CreateJsonRpcIdFilter, HttpBatchJsonRpcRequest,
+    JsonRequestConversionError, JsonResponseConversionError, JsonRpcRequest,
+};
+use canhttp::http::{
+    FilterNonSuccessfulHttpResponseError, HttpRequestConversionError, HttpResponseConversionError,
+};
 use canhttp::observability::ObservabilityLayer;
 use canhttp::{
     convert::ConvertRequestLayer,
@@ -13,9 +20,8 @@ use canhttp::{
         FilterNonSuccessfulHttpResponse, HttpRequestConverter, HttpResponseConverter,
     },
     retry::DoubleMaxResponseBytes,
-    ConvertServiceBuilder,
+    ConvertServiceBuilder, HttpsOutcallError, IcError,
 };
-use error::HttpClientError;
 use evm_rpc_types::RpcError;
 use http::{header::CONTENT_TYPE, HeaderValue, Request as HttpRequest, Response as HttpResponse};
 use ic_cdk::management_canister::{
@@ -23,7 +29,6 @@ use ic_cdk::management_canister::{
 };
 use observability::ObserveHttpCall;
 use serde::{de::DeserializeOwned, Serialize};
-use std::fmt::Debug;
 use tower::{
     layer::util::{Identity, Stack},
     retry::RetryLayer,
@@ -36,15 +41,25 @@ pub mod error;
 pub mod legacy;
 mod observability;
 
-pub fn client<Request, Response>(
+pub fn client<Request, Response, Error>(
     retry: bool,
 ) -> impl Service<HttpRequest<Request>, Response = HttpResponse<Response>, Error = RpcError>
 where
     HttpRequest<Request>: GenerateRequestId,
     (Request, Response): JsonRpcCall<Request, Response>,
-    (Request, Response, HttpClientError): ObserveHttpCall<Request, Response, HttpClientError>,
-    Request: Debug + Serialize + Clone,
-    Response: Debug + DeserializeOwned,
+    (Request, Response, Error): ObserveHttpCall<Request, Response, Error>,
+    Request: Serialize + Clone,
+    Response: DeserializeOwned,
+    RpcError: From<Error>,
+    Error: From<IcError>
+        + From<JsonRequestConversionError>
+        + From<HttpRequestConversionError>
+        + From<ChargeCallerError>
+        + From<HttpResponseConversionError>
+        + From<FilterNonSuccessfulHttpResponseError<Vec<u8>>>
+        + From<JsonResponseConversionError>
+        + From<ConsistentResponseIdFilterError>
+        + HttpsOutcallError,
 {
     let maybe_retry = if retry {
         Some(RetryLayer::new(DoubleMaxResponseBytes))
@@ -59,14 +74,14 @@ where
         None
     };
     ServiceBuilder::new()
-        .map_err(|e: HttpClientError| RpcError::from(e))
+        .map_err(|e: Error| RpcError::from(e))
         .option_layer(maybe_retry)
         .option_layer(maybe_unique_id)
         .layer(
             ObservabilityLayer::new()
-                .on_request(<(Request, Response, HttpClientError)>::observe_request)
-                .on_response(<(Request, Response, HttpClientError)>::observe_response)
-                .on_error(<(Request, Response, HttpClientError)>::observe_error),
+                .on_request(<(Request, Response, Error)>::observe_request)
+                .on_response(<(Request, Response, Error)>::observe_response)
+                .on_error(<(Request, Response, Error)>::observe_error),
         )
         .filter_response(CreateJsonRpcIdFilter::new())
         .layer(service_request_builder())
@@ -74,7 +89,7 @@ where
         .convert_response(FilterNonSuccessfulHttpResponse)
         .convert_response(HttpResponseConverter)
         .convert_request(CyclesAccounting::new(charging_policy_with_collateral()))
-        .service(canhttp::Client::new_with_error::<HttpClientError>())
+        .service(canhttp::Client::new_with_error::<Error>())
 }
 
 pub trait GenerateRequestId: Sized {
@@ -117,13 +132,13 @@ type JsonRpcServiceBuilder<I> = ServiceBuilder<
 /// Middleware that takes care of transforming the request.
 ///
 /// It's required to separate it from the other middlewares, to compute the exact request cost.
-pub fn service_request_builder<I>() -> JsonRpcServiceBuilder<I> {
+pub fn service_request_builder<Request>() -> JsonRpcServiceBuilder<Request> {
     ServiceBuilder::new()
         .insert_request_header_if_not_present(
             CONTENT_TYPE,
             HeaderValue::from_static(CONTENT_TYPE_VALUE),
         )
-        .convert_request(JsonRequestConverter::<I>::new())
+        .convert_request(JsonRequestConverter::<Request>::new())
         .convert_request(HttpRequestConverter)
 }
 
