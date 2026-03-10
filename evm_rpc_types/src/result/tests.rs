73711d6bfa57ab0ab8e9de0ac5d0ccb0a272db48
gregorydemay <gregory.demay@dfinity.org>
2026-03-10 14:00:16 +0100

    DEFI-2665: backwards-compatibility test

--- evm_rpc_types/src/result/tests.rs
+++ evm_rpc_types/src/result/tests.rs
@@ -1,5 +1,6 @@
-use crate::result::{ProviderError, RpcError};
+use crate::result::{BatchResult, ProviderError, RpcError, RpcResult};
 use crate::{EthMainnetService, MultiRpcResult, RpcService, ValidationError};
+use candid::{CandidType, Decode, Deserialize, Encode};
 
 #[test]
 fn test_multi_rpc_result_map() {
@@ -200,4 +201,67 @@ fn test_multi_rpc_result_and_then() {
             "error message".into()
         ))))
     );
+}
+
+mod batch_result_backwards_compatibility {
+    use super::*;
+    use crate::{
+        Block, FeeHistory, LogEntry, Nat256, SendRawTransactionStatus, TransactionReceipt,
+    };
+
+    /// Adding a new variant to `BatchResult` is NOT a breaking change in the Candid API
+    /// because the new variant can only be returned if the corresponding new variant
+    /// was requested in `BatchRequest`, which an old client would never do.
+    ///
+    /// This test verifies that an old client can still decode all pre-existing variants.
+    #[test]
+    fn old_client_can_decode_pre_existing_variants() {
+        let err: RpcError = RpcError::ProviderError(ProviderError::ProviderNotFound);
+        let pre_existing_variants: Vec<BatchResult> = vec![
+            BatchResult::EthFeeHistory(Err(err.clone())),
+            BatchResult::EthGetBlockByNumber(Err(err.clone())),
+            BatchResult::EthGetLogs(Err(err.clone())),
+            BatchResult::EthGetTransactionCount(Err(err.clone())),
+            BatchResult::EthGetTransactionReceipt(Err(err.clone())),
+            BatchResult::EthSendRawTransaction(Err(err)),
+        ];
+        for variant in pre_existing_variants {
+            let encoded = Encode!(&variant).unwrap();
+
+            let decoded = Decode!(&encoded, OldBatchResult).unwrap();
+
+            assert_eq!(decoded, OldBatchResult::try_from(variant).unwrap());
+        }
+    }
+
+    /// `BatchResult` without the `EthCall` variant, simulating a client
+    /// compiled against an older version of the Candid interface.
+    #[derive(Clone, Debug, PartialEq, CandidType, Deserialize)]
+    enum OldBatchResult {
+        EthFeeHistory(RpcResult<FeeHistory>),
+        EthGetBlockByNumber(RpcResult<Block>),
+        EthGetLogs(RpcResult<Vec<LogEntry>>),
+        EthGetTransactionCount(RpcResult<Nat256>),
+        EthGetTransactionReceipt(RpcResult<Option<TransactionReceipt>>),
+        EthSendRawTransaction(RpcResult<SendRawTransactionStatus>),
+    }
+
+    impl TryFrom<BatchResult> for OldBatchResult {
+        type Error = String;
+
+        fn try_from(value: BatchResult) -> Result<Self, Self::Error> {
+            // Exhaustive match ensures a compile-time error when a new variant is added,
+            // reminding the developer to update this test.
+            match value {
+                BatchResult::EthFeeHistory(v) => Ok(Self::EthFeeHistory(v)),
+                BatchResult::EthGetBlockByNumber(v) => Ok(Self::EthGetBlockByNumber(v)),
+                BatchResult::EthGetLogs(v) => Ok(Self::EthGetLogs(v)),
+                BatchResult::EthGetTransactionCount(v) => Ok(Self::EthGetTransactionCount(v)),
+                BatchResult::EthGetTransactionReceipt(v) => Ok(Self::EthGetTransactionReceipt(v)),
+                BatchResult::EthSendRawTransaction(v) => Ok(Self::EthSendRawTransaction(v)),
+                // New variants not present in the old interface.
+                BatchResult::EthCall(_) => Err("EthCall is not supported".to_string()),
+            }
+        }
+    }
 }
